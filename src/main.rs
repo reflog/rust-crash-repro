2d40358b6796a5629eb4197f3bcde46666cb12d7
Eli Yukelzon <reflog@gmail.com>
2021-07-14 12:25:54 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use serde::{Deserialize, Serialize};
+
 #[derive(Serialize, Debug)]
 struct Name {
     field: String,
 }
+
 fn main() {
     let n = Name {
         field: "asd".to_string(),
     };
-    println!("Hello, world! {:?}", n);
+
+    println!("Hello, world! {:}", serde_json::to_string(&n));
 }
